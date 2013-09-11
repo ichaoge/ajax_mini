f78b534a35fb7903705a2ca5911628b241e23893
ichaoge <ichaoge@qq.com>
2013-09-11 14:38:53 +0800

    chaoge
    第一次提交

---
+++ ajaxutil.js
@@ -0,0 +1,54 @@
+/*
+ajaxutil("post","testAjax","cmd=update&uname=123123",true,function(req){
+						//alert("200");
+						var result = req.responseText;
+						//var doc = req.responseXML;
+						document.getElementById("showmsg").innerHTML="<img src='images/82.gif'/>";
+						alert(result);
+						//alert(doc.getElementsByTagName("name")[0].firstChild.data);
+					},function(){
+						document.getElementById("showmsg").innerHTML="<img src='images/loading.gif'/>";
+					} )
+*/
+	function ajaxutil(method,url,data,async,hander200,loading,hander404,hander500){
+			var request = null;
+			if(window.XMLHttpRequest){
+				request = new XMLHttpRequest();
+			}else if(window.ActiveXObject){
+				request = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			request.onreadystatechange=function(){
+				if(4==request.readyState){
+					if(200==request.status){
+						if(hander200){
+							 hander200(request);
+							}
+					}else if(404==request.status){
+						if(hander404){
+							hander404();
+						}
+					}else if(500==request.status){
+						if(hander500){
+							hander500();
+						}
+					}
+				}else{
+					if(loading){
+						loading();
+						}
+				}
+			}
+			if("get"==method.toLowerCase()){
+				if(data==null||data==""){
+					request.open("get",url,async);
+				}else{
+					request.open("get",url+"?"+data,async);
+				}
+				request.send(null);
+			}else if("post"==method.toLowerCase()){
+				request.open("post",url,async);
+				request.setRequestHeader("content-Type","application/x-www-form-urlencoded");
+				request.send(data);
+			}
+			
+		}
